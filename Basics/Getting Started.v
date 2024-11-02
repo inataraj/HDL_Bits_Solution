290079240032c8b74e05ac2b1735258971dd527a
INDRAJITH NATARAJAN <149852010+inataraj@users.noreply.github.com>
2024-11-02 14:39:54 -0700

    Getting Started.v

--- Basics/Getting Started.v
+++ Basics/Getting Started.v
@@ -1,6 +1,6 @@
 module top_module( output one );
 
 // Insert your code here
-    assign one = 1; //assign allows to drive an output continuously with a value
+    assign one = 1; //assign gives an output continuously with a value 1
 
 endmodule
