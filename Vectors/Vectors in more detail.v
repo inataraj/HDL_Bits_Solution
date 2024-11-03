221dfc7b984b1f364ef9606707db05d6db2adcb5
INDRAJITH NATARAJAN <149852010+inataraj@users.noreply.github.com>
2024-11-03 11:38:39 -0800

    Update Vectors in more detail.v

--- Vectors/Vectors in more detail.v
+++ Vectors/Vectors in more detail.v
@@ -1,1 +1,10 @@
 
+`default_nettype none     // Disable implicit nets. Reduces some types of bugs.
+module top_module( 
+    input wire [15:0] in,
+    output wire [7:0] out_hi,
+    output wire [7:0] out_lo );
+  assign out_hi = in [15:8]; //Spliting by assigning it from MSB towards LSB (Upper)
+  assign out_lo = in [7:0];// Spliting by assigning it from MSB towards LSB (Lower)
+
+endmodule
